a050cd029f680645e7083dc0ec2e8518506d5463
tangchaowen <tangcw@belink.com>
2020-09-04 17:16:23 +0800

    测试checkout

--- HiGit.java
+++ HiGit.java
@@ -2,5 +2,6 @@ public class Main{
   public static void main(String[] args){
     System.out.println("hello Git");
     System.out.println("test git");
+    System.out.println("checkout is switch to other branches");
   }
 }
