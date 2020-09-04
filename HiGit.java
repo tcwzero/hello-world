83306d99597c3a42eee2322a77f46fb76f7fbc41
tangchaowen <tangcw@belink.com>
2020-09-04 16:44:14 +0800

    文件修改

--- HiGit.java
+++ HiGit.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main(String[] args){
     System.out.println("hello Git");
-    System.out.println("test git");
+    System.out.println("this is a change");
   }
 }
